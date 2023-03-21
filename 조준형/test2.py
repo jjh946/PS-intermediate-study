b0000ce6e3cf0e08f5a3f56779acaa07736b9205
jjh946 <asdfg3128@gmail.com>
2023-03-21 18:15:43 +0900

    유기농 배추 완

--- 조준형/test2.py
+++ 조준형/test2.py
@@ -1,5 +1,23 @@
 from collections import deque
 
+#입력한 값으로 배추밭 그리드를 만들어준다.
+iteration = int(input())
+
+for i in range(iteration):
+    width, hight,  cabbage_num = input().split()
+    grid = [['0']*hight]*width
+    
+    for j in range(cabbage_num):
+        x, y = input().split()
+        grid[x][y] = '1'
+    
+    print(worms(grid))
+
+
+
+
+
+ # 숫자 섬과 같은 뱅식으로, bfs를 사용하여 모여있는 배추를 확인한다
 def worms(grid):
     number_of_worms = 0
     m = len(grid)    # 세로길이
@@ -17,28 +35,15 @@ def worms(grid):
             for i in range(4):
                 next_x = cur_x + dx[i]
                 next_y = cur_y + dy[i]
-                if next_x >= 0 and next_x <= m-1 and next_y >= 0 and next_y <= n-1:
-                    if grid[next_x][next_y] == 1 and not visited[next_x][next_y]:
+                if next_x >= 0 and next_x < m and next_y >= 0 and next_y < n:
+                    if grid[next_x][next_y] == '1' and not visited[next_x][next_y]:
                         visited[next_x][next_y] = True
                         queue.append((next_x,next_y))
 
     for i in range(m):
         for j in range(n):
-            if grid[i][j] == 1 and visited[i][j] == False:
+            if grid[i][j] == '1' and visited[i][j] == False:
                 bfs(i,j)
                 number_of_worms += 1
 
     return number_of_worms
-
-#입력한 값으로 배추밭 그리드를 만들어준다.
-iteration = int(input())
-
-for i in range(iteration):
-    width, hight,  cabbage_num = map(int, input().split())
-    grid = [[0 for _ in range(hight)] for _ in range(width)]
-    
-    for j in range(cabbage_num):
-        x, y = map(int, input().split())
-        grid[x][y] = 1
-    
-    print(worms(grid))
